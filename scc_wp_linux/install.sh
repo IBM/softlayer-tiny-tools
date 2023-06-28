2136f6c5d415f4ffe8e4566dc6c09f62a15f5dc2
Spark.liu <spark.liu@cn.ibm.com>
2023-06-28 10:51:12 +0800

    add

--- scc_wp_linux/install.sh
+++ scc_wp_linux/install.sh
@@ -32,8 +32,8 @@ install_host_analyzer() {
     chmod +x /usr/bin/host_analyzer
 
     AM_COLLECTOR_ENDPOINT=$API_ENDPOINT
-    check_env.sh AM_COLLECTOR_ENDPOINT "https://$API_ENDPOINT/internal/scanning/scanning-analysis-collector"
-    check_env.sh SCHEDULE "@dailydefault" 
+    ./check_env.sh AM_COLLECTOR_ENDPOINT "https://$API_ENDPOINT/internal/scanning/scanning-analysis-collector"
+    ./check_env.sh SCHEDULE "@dailydefault" 
     ./create_host_analyzer_service.sh
     service host_analyzer start
     service host_analyzer enable
